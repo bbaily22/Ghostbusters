ee58ec14131ec0a0ebd512f7d0daf8ba4e9381b0
bbaily22 <bmb2@williams.edu>
2019-10-13 22:35:22 -0400

    frame

--- PileTree.py
+++ PileTree.py
@@ -1,4 +1,12 @@
-from GameTree import GameTree
+from GameTree import getParity
 
 
 class PileTree:
+    def __init__(self):
+        with open(r"/Users/benbaily/git/Ghostbusters/Scrabble.txt") as f:
+            content = f.readlines()
+        content = [x.strip() for x in content if len(x) > 4]
+        content = {"".join(sorted(x)) for x in content}
+        self.buildTree(content)
+        getParity(self.tree)
+
