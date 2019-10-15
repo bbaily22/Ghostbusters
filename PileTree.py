3cc286cbd4eec62fc884205d17c1cb1864f8f1cf
bbaily22 <bmb2@williams.edu>
2019-10-14 22:16:26 -0400

    Buggy:

--- PileTree.py
+++ PileTree.py
@@ -1,33 +1,43 @@
-# from GameTree import getParity
+from itertools import combinations
 from string import ascii_uppercase
+from collections import namedtuple, OrderedDict
+from pdb import set_trace
+import json
 
 
 class PileTree:
     def __init__(self):
-        with open(r"/Users/benbaily/git/Ghostbusters/Scrabble.txt") as f:
+        with open(r"Scrabble.txt") as f:
             content = f.readlines()
         content = [x.strip() for x in content if len(x) > 4]
         self.buildTree(content)
-        # getParity(self.tree)
 
     def buildTree(self, content):
-        self.tree = {"0": ""}
-        # TODO initialize substrings by reading in from file
+        self.leaves = [{"0": "x", "1": 0}]
+        content = {"".join(sorted(a)) for a in content}
+        substrings = set([])
+        for c in ["BEE", "MUG", "CRY", "DABBING", "GOLDEN", "NEXT"]:
+            for i in range(len(c)):
+                substrings.update(set(combinations(c, i + 1)))
+        index = 1
         for s in substrings:
-            self.tree.update({s: {"0": s}})
-        for k, v in self.tree.items():
+            w = "".join(sorted(s)) + "x"
+            d = {"0": w, "1": index}
+            if "".join(sorted(w)) in content:
+                d.update({"2": True})
+            self.leaves.append(d)
+            index += 1
+        Tree = namedtuple("MyTuple", [t["0"] for t in self.leaves])
+        self.tree = Tree(*self.leaves)
+        for k in self.tree:
             for a in ascii_uppercase:
-                ka = "".join(sorted(k + a))
-                if ka in self.tree:
-                    self.tree[k][a] = self.tree[ka]
+                ka = "".join(sorted(k["0"] + a))
+                if getattr(self.tree, ka, None):
+                    getattr(self.tree, k["0"])[a] = getattr(self.tree, ka)["1"]
+        with open("PileTree.txt", "w") as f:
+            json.dump(self.tree, f)
 
 
 if __name__ == "__main__":
     p = PileTree()
-    k = []
-    for i in range(1, 17):
-        k.append([n for n in p.tree.keys() if len(n) == k])
-    import pdb
-
-    pdb.set_trace()
 
