bb6249960b06dba8542b053e03a124232433bf0e
bbaily22 <bmb2@williams.edu>
2019-10-23 14:08:06 -0400

    Algorithm Finalized

--- Spook.py
+++ Spook.py
@@ -6,8 +6,8 @@ import json
 
 
 class Spook:
-    def __init__(self):
-        with open(r"Scrabble.txt") as f:
+    def __init__(self, file):
+        with open(file) as f:
             content = f.readlines()
         content = [x.strip() for x in content if len(x) > 4]
         self.buildTree(content)
@@ -18,28 +18,60 @@ class Spook:
                 node = self.leaves[j]
                 if node.get("2", False):
                     node["3"] = i % 2 + 1
+                    node["4"] = "2"
                 else:
                     keys = [*node]
                     keys.remove("0")
                     keys.remove("1")
                     if i % 2 == 0:
                         for key in keys:
-                            if self.leaves[node[key]].get("3", None) and self.leaves[node[key]]["3"] == 1:
+                            if (
+                                self.leaves[node[key]].get("3", None)
+                                and self.leaves[node[key]]["3"] == 1
+                            ):
                                 node["3"] = 1
+                                node["4"] = key
                                 break
-                        node["3"] = 2
-                    else: 
+                        if not node.get("3", None):
+                            node["3"] = 2
+                            node["4"] = None
+                    else:
                         for key in keys:
-                            if self.leaves[node[key]].get("3", None) and self.leaves[node[key]]["3"] == 2:
+                            if (
+                                self.leaves[node[key]].get("3", None)
+                                and self.leaves[node[key]]["3"] == 2
+                            ):
                                 node["3"] = 2
+                                node["4"] = key
                                 break
-                        node["3"] = 1
+                        if not node.get("3", None):
+                            node["3"] = 1
+                            node["4"] = None
 
+    def minSolution(self):
+        stack = ["x"]
+        self.minTree = {}
+        while stack:
+            key = stack.pop()
+            if self.minTree.get(key, None):
+                continue
+            if self.tree[key].get("4", None):
+                if not self.tree[key]["4"] == "2":
+                    w = "".join(sorted(key + self.tree[key]["4"]))
+                    stack.append(w)
+                    self.minTree[key] = [w]
+            else:
+                self.minTree[key] = []
+                for k in [*self.tree[key]]:
+                    if k not in ["0", "1", "2", "3", "4"]:
+                        w = "".join(sorted(key + k))
+                        stack.append(w)
+                        self.minTree[key].append(w)
 
     def buildTree(self, content):
         content = {"".join(sorted(a)) for a in content}
         substrings = set([])
-        for c in ["BEE", "MUG", "CRY", "DABBING", "GOLDEN", "NEXT"]:
+        for c in content:
             for i in range(len(c)):
                 substrings.update(set(combinations(c, i + 1)))
         substrings = {"".join(sorted(s)) for s in substrings}
@@ -49,9 +81,7 @@ class Spook:
         self.stems[0].append(0)
         for s in substrings:
             w = "".join(sorted(s)) + "x"
-            d = {"0": w, "1": index}
-            if "".join(sorted(w)) in content:
-                d.update({"2": True})
+            d = {"0": w, "1": index, "2": "".join(sorted(s)) in content}
             self.leaves.append(d)
             self.stems[len(s)].append(index)
             index += 1
@@ -62,11 +92,12 @@ class Spook:
                 if ka in self.tree:
                     self.tree[k][a] = self.tree[ka]["1"]
         self.getParity()
+        self.minSolution()
         with open("Spook.txt", "w") as f:
-            json.dump(self.tree, f)
+            json.dump(self.minTree, f)
 
 
 if __name__ == "__main__":
-    p = Spook()
+    s = Spook("Scrabble.txt")
     set_trace()
 
